3056705a6e76e7aee9a7e120c39b2d7ad1cffd1b
seanwatson <seanwatson@server.fake>
2014-01-18 17:22:23 -0800

    Making it installable via pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools.import setup
+readme = open('README.md').read()
+setup(name='notehub',
+      version='0.1',
+      author='Sean Watson',
+      license='MIT',
+      description='A simple wrapper for the Notehub.org API.',
+      long_description=readme,
+      py_modules=['notehub'])
